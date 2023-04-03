c52b0ba6c5a9a532c184ede81e002fb1ef794a5b
danielhcg <35074368+danielhcg@users.noreply.github.com>
2023-04-03 15:21:16 +0000

    implemented a for loop

--- main.kt
+++ main.kt
@@ -1,3 +1,27 @@
 fun main(args: Array<String>) {
-  println("Hello world!")
+  
+  // calling intro message to prompt user 
+  introMes()
+  
+  
 }
+
+// intro prompt to be displayed to the user 
+fun introMes() {
+  
+  //creating char aray to hold calculator operations
+  val op = charArrayOf('+', '-', '*', '/')
+  
+  // User prompt
+  println("Enter the operation you wish to carry out from the following options")
+
+  // for loop to print out every operation user can carry out 
+  for (oP in op) {
+    print("$oP ")
+  }
+}
+
+// loop constructs 
+// while loop
+// do while 
+// fo each 
