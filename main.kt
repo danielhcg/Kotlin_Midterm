40a04f076e80f9895ac58b390d8c1b97f518a601
danielhcg <35074368+danielhcg@users.noreply.github.com>
2023-04-03 16:57:02 +0000

    worked reading user input and while looping

--- main.kt
+++ main.kt
@@ -1,27 +1,41 @@
 fun main(args: Array<String>) {
-  
-  // calling intro message to prompt user 
+
+  // variable declaration section
+  var userOP: Char // to hold value of operation user wants to use
+  var userST: String
+
+  // calling intro message to prompt user
   introMes()
-  
-  
+
+  // reading in the stringvariable of the user to carry out a specific operation
+  userST = readLine()
+
+  // while loop to ask user to continually re-enter selection acceptable operation is entered
+  while (userST.isEmpty()) {
+    print("Invalid Entry. Please try again")
+    userST = readLine()
+  }
+  userOP = userST.first()
+
+  print("You selected " + userOP + "is this correct?")
 }
 
-// intro prompt to be displayed to the user 
+// intro prompt to be displayed to the user
 fun introMes() {
-  
-  //creating char aray to hold calculator operations
+
+  // creating char aray to hold calculator operations
   val op = charArrayOf('+', '-', '*', '/')
-  
+
   // User prompt
   println("Enter the operation you wish to carry out from the following options")
 
-  // for loop to print out every operation user can carry out 
+  // for loop to print out every operation user can carry out
   for (oP in op) {
     print("$oP ")
   }
 }
 
-// loop constructs 
+// loop constructs
 // while loop
-// do while 
-// fo each 
+// do while
+// fo each
