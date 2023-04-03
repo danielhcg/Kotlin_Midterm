1e262907ab3f8efc2890657df84cb7c06a098f69
danielhcg <35074368+danielhcg@users.noreply.github.com>
2023-04-03 22:03:10 +0000

    implemented do while loop working with ui, implemented lambda expression, and implemented a forEach loop

--- main.kt
+++ main.kt
@@ -1,23 +1,53 @@
-fun main(args: Array<String>) {
+fun main() {
 
-  // variable declaration section
-  var userOP: Char // to hold value of operation user wants to use
-  var userST: String
+  // boolean intialized to true to store value to rerun program
+  var again = true;
 
-  // calling intro message to prompt user
-  introMes()
+  while (again) { // loop to ask user if they wish to carry out another operatoin 
 
-  // reading in the stringvariable of the user to carry out a specific operation
-  userST = readLine()
+    // calling intro message to prompt user
+    introMes()
 
-  // while loop to ask user to continually re-enter selection acceptable operation is entered
-  while (userST.isEmpty()) {
-    print("Invalid Entry. Please try again")
-    userST = readLine()
+    println("\n\n")
+
+    // read user input into val 
+    val userOP = readLine()!!.first()
+
+    var isCor: String? = null // declare isCor variable to deterime if user entered correct numbers 
+
+    // validating that the user entered the correct number using a do while loop 
+    do {
+
+      // asking user for 2 numjbers to do an operation on 
+      println("\nPlease enter the first number you would like to perform an operation on:")
+      var x = readLine()?.toIntOrNull() ?: 0 // reading each number into variables
+      println("\nPlease enter the first number you would like to perform an operation on: ")
+      var y = readLine()?.toIntOrNull() ?: 0 // reading each number into variables
+
+      // updating value is isCor to check if everything is Gucci 
+      println("You entered $x and $y is that correct? (Y/N)")
+      isCor = readLine()
+    } while (isCor == "N" || isCor == "n")
+
+    println("We need to choose a pokemon to carry out the operation. Which is do you choose?") // prompting user 
+    val pokemon = listOf("Charizard", "Venusaur", "Blastoise")  // list of stirng pokemon for user to choose from 
+    // executing a lambda expression and iterating over the list to display each 
+    pokemon.forEach { pkm -> 
+      println(pkm)
+    }
+    
+    
+    // aksing user if they would like to re-run the program 
+    println("Would you like to perform another calculation? (Y/N)") 
+    var ui = readLine()
+    if (ui == "Y" || ui == "y") {
+      again = true
+    } else {
+      again = false
+    }
   }
-  userOP = userST.first()
-
-  print("You selected " + userOP + "is this correct?")
+  println("Thanks for using my program! Bye!")
+    
 }
 
 // intro prompt to be displayed to the user
@@ -36,6 +66,17 @@ fun introMes() {
 }
 
 // loop constructs
-// while loop
-// do while
 // fo each
+
+
+// reasons why i think i deserve an A
+/*
+i ask the user if they want to re run the program using a while loop 
+i worked with user input 
+i commented my code
+
+
+
+
+
+*/
